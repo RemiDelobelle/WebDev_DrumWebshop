7acc55bf48314723f5fc12337099019f7c1efbc7
RemiDelobelle <r0903204@student.vives.be>
2024-03-30 21:46:44 +0100

    Add Authentication roles

--- Data/DrumContext.cs
+++ Data/DrumContext.cs
@@ -1,9 +1,11 @@
-﻿using DrumWebshop.Models;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using DrumWebshop.Models;
 
 namespace DrumWebshop.Data
 {
-    public class DrumContext: DbContext
+    public class DrumContext: IdentityDbContext<IdentityUser>
     {
         public DrumContext(DbContextOptions<DrumContext> options) : base(options)
         {
@@ -20,8 +22,13 @@ namespace DrumWebshop.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
+
             modelBuilder.Entity<Hardware>()
                 .OwnsMany(h => h.HwComponents); // Configure HwComponents as a complex type owned by Hardware
+
+            modelBuilder.Entity<IdentityUserLogin<string>>()
+                .HasKey(l => new { l.LoginProvider, l.ProviderKey });
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
--- Program.cs
+++ Program.cs
@@ -1,45 +1,93 @@
 using DrumWebshop;
-using DrumWebshop.Data;
 using DrumWebshop.Models;
+using DrumWebshop.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using DrumWebshop.Data;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-// Add database context
-builder.Services.AddDbContext<DrumContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseExceptionHandler("/Home/Error");
+    public static async Task Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add database context
+        builder.Services.AddDbContext<DrumContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+        builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+        .AddRoles<IdentityRole>()
+        .AddEntityFrameworkStores<DrumContext>();
+
+        // Add services to the container.
+        builder.Services.AddControllersWithViews();
+        builder.Services.AddRazorPages();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+        }
+        app.UseStaticFiles();
+
+        app.UseStatusCodePages();
+
+        app.UseRouting();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+
+        using (var roleScope = app.Services.CreateScope())
+        {
+            var roleManager = roleScope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+            var roles = new[] { "Admin", "Manager", "Member", "PendingMember" };
+
+            foreach (var role in roles)
+            {
+                if (!await roleManager.RoleExistsAsync(role))
+                    await roleManager.CreateAsync(new IdentityRole(role));
+            }
+        }
+
+        using (var userScope = app.Services.CreateScope())
+        {
+            var userManager = userScope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+            string email = "admin@admin.com";
+            string password = "Test1234*";
+
+            if (await userManager.FindByEmailAsync(email) == null)  // account not found --> create
+            {
+                var user = new IdentityUser();
+                user.UserName = email;
+                user.Email = email;
+
+                await userManager.CreateAsync(user, password);
+
+                await userManager.AddToRoleAsync(user, "Admin");
+            }
+        }
+
+
+        //// Add items to database
+        //using var scope = app.Services.CreateScope();
+        //var serviceProvider = scope.ServiceProvider;
+        //var context = serviceProvider.GetRequiredService<TestContext>();
+
+        //List<Product> products = DbInitializer.InitializeProducts(context);
+        //context.Products.AddRange(products);
+        //context.SaveChanges();
+
+        app.Run();
+    }
 }
-app.UseStaticFiles();
 
-app.UseStatusCodePages();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-
-//// Add items to database
-//using var scope = app.Services.CreateScope();
-//var serviceProvider = scope.ServiceProvider;
-//var context = serviceProvider.GetRequiredService<DrumContext>();
-
-//List<Product> products = DbInitializer.InitializeProducts(context);
-//context.Products.AddRange(products);
-//context.SaveChanges();
-
-
-app.Run();
